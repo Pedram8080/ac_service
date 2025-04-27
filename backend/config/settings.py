040068434489bf561b4c9c156f68c001f9f5b997
pedram <pedramaslani8081@gmail.com>
2025-04-27 19:08:55 +0330

    setting update

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -79,10 +79,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ac_db',
-        'USER': 'ac_user',
-        'PASSWORD': 'ac_pass',
-        'HOST': 'db',
+        'NAME': 'ac_service',
+        'USER': 'admin',
+        'PASSWORD': '12054801',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
