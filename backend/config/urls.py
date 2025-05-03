71fedd97054e5465347e344df3b09d124deb2b3d
pedram <pedramaslani8081@gmail.com>
2025-05-03 09:44:01 +0330

    gi

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from service.views import home
+from django.contrib.sitemaps.views import sitemap
+from service.sitemaps import StaticViewSitemap
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('service.urls')),
     # path('sms/', include('service.urls')), comment
     path('', home, name='home'),
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
 ]
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
