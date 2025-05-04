1bcd867fb8ccd417bd77f98935e81283df3ce3a7
pedram <pedramaslani8081@gmail.com>
2025-05-04 20:01:15 +0330

    gi

--- backend/service/views.py
+++ backend/service/views.py
@@ -2,7 +2,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RequestSerializer
-from kavenegar import *
 from django.conf import settings
 from .models import Request
 import requests
