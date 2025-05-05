ba80fd1ce152bb31ac0863b6e119664030ba1aef
Pedram Aslani <pedramaslani8081@gmail.com>
2025-05-05 13:47:39 +0330

    gi

--- init-db.sh
+++ init-db.sh
@@ -2,7 +2,6 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER ac_user WITH PASSWORD 'ac_pass';
-    CREATE DATABASE ac_db;
-    GRANT ALL PRIVILEGES ON DATABASE ac_db TO ac_user;
+    ALTER USER postgres WITH PASSWORD 'admin123';
+    GRANT ALL PRIVILEGES ON DATABASE ac_db TO postgres;
 EOSQL 
