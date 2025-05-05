02b7e0a994dac4808e5a0db4ef9601a8d07e24b8
pedram <pedramaslani8081@gmail.com>
2025-05-05 12:10:20 +0330

    gi

--- backend/service/urls.py
+++ backend/service/urls.py
@@ -8,6 +8,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('request/', CreateRequestView.as_view(), name='create-request'),
     path('', views.home, name='home'),
+    path('sms/send-request/', request_specialist, name='request_specialist'),
     path('panel/login/', auth_views.LoginView.as_view(template_name='service/panel_login.html'), name='login'),
     path('panel/logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('panel/', views.request_panel_view, name='panel'),
